998a91c79a996612b5eb3229beddc6c927364194
Rovasoanirina Princy Rakotosalama <rrakotos@c1r9s11.42antananarivo.mg>
2024-07-09 16:31:06 +0300

    send another

--- check_input.c
+++ check_input.c
@@ -6,7 +6,7 @@
 /*   By: rrakotos <rrakotos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 13:30:47 by rrakotos          #+#    #+#             */
-/*   Updated: 2024/07/09 15:22:22 by rrakotos         ###   ########.fr       */
+/*   Updated: 2024/07/09 16:29:08 by rrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,37 @@ static char	*ft_strjoin_data(char *s1, char *s2)
 	tmp_s1 = s1;
 	while (*tmp_s1)
 		*(new_s++) = *(tmp_s1++);
-	if (s1)
-        *(new_s++) = ' ';
+	if (*s1 == 0 && *s2)
+		*(new_s++) = ' ';
 	while (*s2)
 		*(new_s++) = *(s2++);
-	free(s1);
+	// free(s1);
 	return (new_s - len - 1);
-}
-
-static int check_dup(char *s)
-{
-    
 }
 
 static int	check_number(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
-        i++;
-    if (s[i] == '-' || s[i] == '+')
-        i++;
-    while (s[i] >= '0' && s[i] <= '9')
-        i++;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (ft_isalpha(s[i]))
+			return (0);
+		while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+			i++;
+		if (s[i] == '-' || s[i] == '+')
+		{
+			i++;
+			if (s[i] >= 48 && s[i] <= 57)
+				check_number(s + i);
+			else
+				return (0);
+		}
+		while (s[i] >= '0' && s[i] <= '9')
+			i++;
+	}
+	return (1);
 }
 
 char	*check_input(char **argv)
@@ -62,5 +69,6 @@ char	*check_input(char **argv)
 	data = ft_calloc(1, 1);
 	while (argv[i])
 		data = ft_strjoin_data(data, argv[i++]);
+	// printf("%d\n", check_number(data));
 	return (data);
 }
--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,28 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rrakotos <rrakotos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/09 13:41:46 by rrakotos          #+#    #+#             */
-/*   Updated: 2024/07/09 16:16:29 by rrakotos         ###   ########.fr       */
+/*   Created: 2024/02/19 10:16:27 by rrakotos          #+#    #+#             */
+/*   Updated: 2024/07/09 16:16:36 by rrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stddef.h>
-# include <stdio.h>
-
-int		ft_atoi(const char *nptr);
-int		ft_isdigit(int c);
-char	**ft_split(char const *s, char c);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-void	*ft_calloc(size_t elementCount, size_t elementSize);
-void	ft_bzero(void *s, size_t n);
-int		ft_isalpha(int c);
-
-#endif
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (1);
+	return (0);
+}
