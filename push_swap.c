9e967edb8709bce940c82a423e4b9955a270a847
Rovasoanirina Princy Rakotosalama <rrakotos@c1r9s9.42antananarivo.mg>
2024-07-25 16:35:15 +0300

    send instruction for my algo

--- push_swap.c
+++ push_swap.c
@@ -6,31 +6,38 @@
 /*   By: rrakotos <rrakotos@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 15:37:11 by rrakotos          #+#    #+#             */
-/*   Updated: 2024/07/25 15:37:12 by rrakotos         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:33:35 by rrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void    push_swap(t_stack **a)
+// return number of ra
+// int sortable(t_stack *a)
 // {
-//     int len;
-//     if (a)
-//     {
-//         if (!*a || is_asc(*a))
-//             return ;
-//         len = count_node(*a);
-//         if (len == 2 && (*a)->value > (*a)->next->value)
-//         {
-//             swap(a, 'a');
-//             return ;
-//         }
-//         // else if (len == 3)
-//         // else if (len == 4)
-//         // else if (len == 5)
-//         // else 
-//     }
+//     t_stack *node_min;
+//     t_stack *node_top;
+    
+//     node_min = ;
+//     node_top = ;
+//     // ambany node_min is_asc
+//     // ambany node_top is asc jusqu'a node_min
 // }
+
+void    push_swap(t_stack **a, t_stack **b)
+{
+    int len_a;
+
+    len_a = count_node(*a);
+    if (len_a < 3 && !is_asc(*a))
+        exec_move(swap, a, "sa", 1);
+    else if (len_a == 3 && !is_asc(*a))
+        sort_three(a);
+    else if (len_a > 3 && sortable())
+        // tant que is_asc(*a) -> false => exec_move(rotate, ra, a, sortable());
+    // else if (len_a > 3 && !sortable())  
+        // mouvement ao anaty main no ataony
+}
 
 // sort_stack(t_stack **a)
 // {
