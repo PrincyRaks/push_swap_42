85d69bc8fa44685bec1117096baad8771566077b
Rovasoanirina Princy Rakotosalama <rrakotos@c1r9s9.42antananarivo.mg>
2024-08-06 08:58:21 +0300

    optimize cost

--- mandatory/range_algo.c
+++ mandatory/range_algo.c
@@ -90,7 +90,7 @@ void	range_algo(t_stack **a, t_stack **b)
 {
 	t_stack	*min_cost;
 
-	while (count_node(*a) > 3 || !is_asc(*a) || !sortable(*a))
+	while (count_node(*a) > 3 && !is_asc(*a) && !sortable(*a))
 	{
 		min_cost = get_min_cost_a(*a, *b);
 		push_to_b(min_cost, a, b);
